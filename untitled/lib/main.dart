89ef318803d8728df7493c0eed2ee78ed2ab312b
aly22 <34962997+aly22@users.noreply.github.com>
2022-06-27 20:28:47 +0300

    Iamrichapp

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("data"),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/images2/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
